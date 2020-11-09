563f282adc7285f97c2e223232d9aabae96d270f
ygasd <73930478+ygasd@users.noreply.github.com>
2020-11-09 16:35:40 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,9 +1,11 @@
 // Scan
 function scan(){
   //const SERVICE_UUID = 0x180f;
-  navigator.bluetooth.requestDevice({
-    filters: [{
-      name: 'Blank',
-    }]
-  })
+  //navigator.bluetooth.requestDevice({
+    //filters: [{
+      //name: 'Blank',
+    //}]
+  //})
+  navigator.bluetooth.requestDevice({ acceptAllDevices:true })
+    .catch(error => console.log(error));
 }
