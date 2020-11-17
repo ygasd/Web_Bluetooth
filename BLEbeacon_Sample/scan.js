cbc8620a3b9e5e8325cf167df451e8ade36a96ec
ygasd <73930478+ygasd@users.noreply.github.com>
2020-11-17 16:28:33 +0900

    Update scan.js

--- BLEbeacon_Sample/scan.js
+++ BLEbeacon_Sample/scan.js
@@ -65,4 +65,16 @@ const logDataView = (labelOfDataSource, key, valueDataView) => {
   log(`  ${labelOfDataSource} Data: ` + key +
       '\n    (Hex) ' + hexString +
       '\n    (ASCII) ' + asciiString);
+
+  log = ChromeSamples.log;
+
+  function isWebBluetoothEnabled() {
+    if (navigator.bluetooth) {
+      return true;
+    } else {
+      ChromeSamples.setStatus('Web Bluetooth API is not available.\n' +
+          'Please make sure the "Experimental Web Platform features" flag is enabled.');
+      return false;
+    }
+  }
 };
